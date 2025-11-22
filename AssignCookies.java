09f91a91ceec4de1aeae467a9d755436a2cd2ea0
Pranshu Nigam <2k23.csaiml2313503@gmail.com>
2025-11-22 14:56:53 +0530

    Add LeetCode solutions

---
+++ AssignCookies.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int findContentChildren(int[] g, int[] s) {
+
+        int i = 0;  //child
+        int j = 0;  //cookie
+
+        Arrays.sort(g);
+        Arrays.sort(s);
+
+        while(i<g.length && j<s.length){
+            if(s[j]>=g[i]){
+                i++;
+                j++;
+            }
+            else
+            j++;
+        }
+        return i;
+    }
+}
